c0df57033f5209ebdfe48aa330340ec041274fcc
rinChome <105960985+rinChome@users.noreply.github.com>
2022-06-11 16:49:25 +0900

    Add files via upload

---
+++ js/js1.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function getData() {
+  var product = '';
+  var selectData = product = document.getElementById("select-products");
+  product = selectData.value;
+  console.log(product);
+
+}
